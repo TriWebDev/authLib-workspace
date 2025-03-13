d933877ab203d3559008c3422185d299487f2dc3
DavMunHer <davidmunyozh@gmail.com>
2025-03-13 23:06:12 +0100

    fix(feat): Now able to change between themes

--- projects/auth/src/lib/components/auth/auth.component.ts
+++ projects/auth/src/lib/components/auth/auth.component.ts
@@ -1,7 +1,14 @@
-import { Component, input, signal, ViewEncapsulation, ElementRef, Renderer2 } from '@angular/core';
+import {
+    Component,
+    input,
+    signal,
+    ViewEncapsulation,
+    ElementRef,
+    OnInit,
+} from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ToggleComponent } from "./toggle/toggle.component";
+import { ToggleComponent } from './toggle/toggle.component';
 import { NgClass } from '@angular/common';
 
 @Component({
@@ -9,52 +16,54 @@ import { NgClass } from '@angular/common';
     imports: [LoginComponent, RegisterComponent, ToggleComponent, NgClass],
     templateUrl: './auth.component.html',
     styleUrl: './auth.component.css',
-    encapsulation: ViewEncapsulation.ShadowDom
+    encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class AuthComponent {
-    public theme = input<"light" | "dark" | "default">("dark");
-
-    constructor(private el: ElementRef, private renderer: Renderer2) {
-
-    }
-    ngOnInit(): void {
-        this.updateTheme();
-    }
+export class AuthComponent implements OnInit {
+    public theme = input<'classicB&W' | 'NeoViolet'>('classicB&W');
     // The showTemplate variable is for showing an extra component (toggle) in bigger devices, for changing between forms
     protected showTemplate = signal<'login' | 'register'>('register');
-
     // In smaller devices, the previous logic is managed only through the register and login components, through opacity
     protected showForm = signal<'login' | 'register'>('login');
 
-    updateTheme() {
+    constructor(private el: ElementRef) { }
+
+    /* Logic when initialising component */
+    ngOnInit(): void {
+        this.updateTheme();
+    }
+
+    private updateTheme() {
         const themeColors = this.getThemeColors();
         this.setComponentStyles('--primary-color', themeColors[0]);
         this.setComponentStyles('--secondary-color', themeColors[1]);
         this.setComponentStyles('--input-color', themeColors[2]);
     }
 
-    getThemeColors() {
+    private getThemeColors() {
         switch (this.theme()) {
-            case 'light':
-                return ['#fff', '#000', '#999']
-            case 'dark':
-                return ['#000', '#fff', '#999']
+            case 'classicB&W':
+                return ['#111', '#fff', '#ddd'];
+
+            // return ['#fff', '#000', '#999']
+            case 'NeoViolet':
             default:
-                return ['#372aac', '#fff', '#e5e7eb']
+                return ['#372aac', '#fff', '#e5e7eb'];
         }
     }
 
-    setComponentStyles(variableName: string, color: string) {
+    private setComponentStyles(variableName: string, color: string) {
         const hostElement = this.el.nativeElement;
-        this.renderer.setStyle(hostElement, variableName, color);
+        // this.renderer.setStyle(hostElement, variableName, color);
+        hostElement.style.setProperty(variableName, color);
     }
+    /* End logic when initialising component */
 
-    changeForm(form: 'register' | 'login') {
+    /* Events logic */
+    protected changeForm(form: 'register' | 'login') {
         this.showForm.set(form);
     }
 
-
-    changeTemplate(template: 'login' | 'register') {
+    protected changeTemplate(template: 'login' | 'register') {
         this.showTemplate.set(template);
     }
 }
